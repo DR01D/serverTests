1259431fc9c6921d329ffcf0c20ac73dd5ea67bd
Al Rosetti <cs@cleanpaw.com>
2018-09-15 14:29:06 -0700

    9.15.08.2.29pm

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
@@ -10,13 +8,11 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/test/:name", (req, res) => {
-    var testName = req.params.name;
-    res.send(testName);
+app.get("/test/", (req, res) => {
+    res.send('hello world');
 });
 
 app.use(express.static("public"));
-app.use(express.static("public/images"));
 
 app.listen(port, () => {
     console.log('Server listening on port ' + port);

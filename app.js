5091e004957030bfa4d6c2e76f463aee704bcf16
Al Rosetti <cs@cleanpaw.com>
2018-09-17 10:47:10 -0700

    9.17.18.10.47am

--- app.js
+++ app.js
@@ -3,17 +3,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     // res.send('send this');
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get("/test", (req, res) => {
-    res.redirect('https://www.google.com');
-    // res.send(req.accepts(['json', 'text']));
+app.get('/test', (req, res, next) => {
+    if (req.query.name != 'cat') {
+        next();
+        return;
+    } 
+    res.send('it was cat');
 });
 
-app.use(express.static("public"));
+app.get('/test', (req, res) => {
+    // res.send('it was not cat');
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
